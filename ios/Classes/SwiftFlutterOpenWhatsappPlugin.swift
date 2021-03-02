7e4313a9e44052f87a87b963166369e311435b0e
Guillermo Jeremy De La Cruz Onton <guillermo.dlco@outlook.com>
2021-03-02 12:59:20 -0500

    Fix open whatsapp ios

--- ios/Classes/SwiftFlutterOpenWhatsappPlugin.swift
+++ ios/Classes/SwiftFlutterOpenWhatsappPlugin.swift
@@ -12,26 +12,18 @@ public class SwiftFlutterOpenWhatsappPlugin: NSObject, FlutterPlugin {
         if(call.method == "getPlatformVersion"){
             result("iOS " + UIDevice.current.systemVersion)
         } else if(call.method == "sendSingleMessage"){
-            
             DispatchQueue.main.async{
-                
-                //let mobileNo: String = args["mobileNo"];
-                //let message: String = args["message"];
-                
                 guard let args = call.arguments else {
                     return
                 }
                 if let myArgs = args as? [String: Any],
                     let mobileNo = myArgs["mobileNo"] as? String,
                     let message = myArgs["message"] as? String {
-                    //result("Params received on iOS = \(someInfo1), \(someInfo2)")
                     let url: String = ("https://wa.me/" + mobileNo + "?text=" + (message.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""))
                     if let whatsAppUrl = URL(string: url) {
                         if UIApplication.shared.canOpenURL(whatsAppUrl) {
                             UIApplication.shared.openURL(whatsAppUrl)
                         }else {
-                            //let errorAlert = UIAlertView(title: "Sorry", message: "You can't send a message to this number", delegate: self, cancelButtonTitle:"Ok")
-                            // errorAlert.show()
                             result("Please, install whatsapp")
                         }
                     } else {
